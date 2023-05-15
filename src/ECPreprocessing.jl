be48940e7d49edb71ab77abc44cb1dc8cd315586
Tommaso Ferrucci <64267107+TomFer97@users.noreply.github.com>
2023-05-15 17:05:42 +0200

    User load profile creation

--- src/ECPreprocessing.jl
+++ src/ECPreprocessing.jl
@@ -5,25 +5,28 @@ using DataFrames
 
 # Define the file path
 general_path = @__DIR__
-relative_path_GSE = "GSE_prelievo.xlsx"
+relative_path_GSE = "../data/GSE_prelievo.xlsx"
 file_GSE= joinpath(general_path,relative_path_GSE)
 
 # Read data from the first sheet
 #sheet_name = XLSX.sheetnames(file_GSE)
-GSE_profiles= XLSX.readdata(file_GSE,"Foglio1","A1:S8761")
+GSE_profiles= DataFrame(XLSX.readtable(file_GSE,"Foglio1"))
 
 # read YAML file
-yaml_file_name = "energy_community_model.yml"
+yaml_file_name = "../data/energy_community_model.yml"
 file_YAML = joinpath(@__DIR__,yaml_file_name)
 config = YAML.load_file(file_YAML)
-yaml_file_out = "Pre_processing/processed_energy_community_model.yml"
+yaml_file_out = "output/processed_energy_community_model.yml"
 
 # definition of main parameters
 final_step = 8760 #or any other time res resolution
-n_users_residential = 3
-n_users_other_uses = 4
-residential_yearly = [2700, 3200, 4000]
-other_uses = [1300, 2000, 1000, 4000]
+start_users_residential = 1
+end_users_residential = 3
+start_users_other_uses = 4
+end_users_other_uses = 9
+residential_yearly = fill(2700,end_users_residential)
+other_uses_yearly = fill(3400,end_users_other_uses-start_users_other_uses+1)
+consumption_yearly = vcat(residential_yearly,other_uses_yearly)
 
 # definition of residential and commercial compontens
 residential_components = Dict(
@@ -123,7 +126,7 @@ commercial_components = Dict(
 users = Dict()
 components = ["market_set","PV", "batt", "conv", "load"]
     
-for i = 1: n_users_residential
+for i = start_users_residential: end_users_residential
     user_name = "user$i"
     users[user_name] = Dict()
     for component = components
@@ -135,7 +138,7 @@ for i = 1: n_users_residential
     push!(config["general"]["user_set"],user_name)
 end
 
-for i = 1 + n_users_residential : n_users_residential + n_users_other_uses
+for i = start_users_other_uses : end_users_other_uses
     user_name = "user$i"
     users[user_name] = Dict()
     for component = components
@@ -154,20 +157,25 @@ config["users"] = users
 input_dataframe = DataFrame(time = [i for i in 1:final_step])
 
 # create user profiles from type of users
-for i = 1: n_users_residential
-    residential_code = "PDMM"
-    residential_monthly = residential_yearly/12
+for i = start_users_residential: end_users_other_uses
     label = "load_user$i"
-
-end
-
-for i = n_users_residential + 1 : n_users_other_uses + n_users_residential
-    residential_code = "PAUM"
+    if i < start_users_other_uses
+        code = Symbol("PDMM")
+        user_residential_monthly = fill(consumption_yearly[i]/12,final_step)
+        user_load_y= user_residential_monthly.*GSE_profiles[:,code]
+        input_dataframe[!,label] = user_load_y
+    end
+    if i > end_users_residential
+        code = Symbol("PAUM")
+        user_other_uses_monthly = fill(consumption_yearly[i]/12,final_step)
+        user_load_y= user_other_uses_monthly.*GSE_profiles[:,code]
+        input_dataframe[!,label] = user_load_y
+    end
 end
 
 
 # create a CSV file named "filename.csv" with a header row and the values in the array
-CSV.write("Pre_processing/input_resources.csv", input_dataframe)
+CSV.write("output/input_resources.csv", input_dataframe)
 
 
 # write updated configuration to YAML file
